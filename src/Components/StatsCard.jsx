c2ee9d84e712dd3e5fbb34fb7b29f2bcee097a37
Daniel Amekpoagbe <amekpoagbedaniel@gmail.com>
2025-02-23 00:38:58 +0000

    sorting

---
+++ src/Components/StatsCard.jsx
@@ -0,0 +1,30 @@
+// const StatsCard = () => {
+
+//     <>
+//       {/* Stats Section */}
+//       <section className="px-6 md:px-10 py-10 bg-gray-800 flex flex-col md:flex-row justify-around text-center gap-6 md:gap-0">
+//         <StatsCard number="300+" label="Happy Clients" />
+//         <StatsCard number="900+" label="Amazing Projects" />
+//         <StatsCard number="25+" label="Years Operated" />
+//       </section>
+
+//       /**
+//  * StatsCard Component
+//  *
+//  * Displays individual statistics in a structured format.
+//  * @param {string} number - The number/statistic to display.
+//  * @param {string} label - The label describing the statistic.
+//  */
+// const StatsCard = ({ number, label }) => {
+//   return (
+//     <div className="space-y-2">
+//       <h3 className="text-3xl font-bold text-blue-400">{number}</h3>
+//       <p className="text-gray-400">{label}</p>
+//     </div>
+//   );
+// };
+//     </>
+
+// };
+
+// export default StatsCard;
